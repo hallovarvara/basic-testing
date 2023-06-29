b22450a19f1fa3e49ddb950671cd852e759d5fb4
Varya Deviaterikova <hallovarvara@gmail.com>
2023-06-29 22:21:08 +0100

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,78 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+import { TEST_TIMEOUT } from '../lib/constants';
 
 describe('simpleCalculator tests', () => {
-  test('should add two numbers', () => {
-    // Write your test here
-  });
+  test(
+    'should add two numbers',
+    () => {
+      expect(simpleCalculator({ a: 5, b: 19, action: Action.Add })).toBe(24);
+    },
+    TEST_TIMEOUT,
+  );
 
-  test('should subtract two numbers', () => {
-    // Write your test here
-  });
+  test(
+    'should subtract two numbers',
+    () => {
+      expect(simpleCalculator({ a: 5, b: 19, action: Action.Subtract })).toBe(
+        -14,
+      );
+    },
+    TEST_TIMEOUT,
+  );
 
-  test('should multiply two numbers', () => {
-    // Write your test here
-  });
+  test(
+    'should multiply two numbers',
+    () => {
+      expect(simpleCalculator({ a: 5, b: 19, action: Action.Multiply })).toBe(
+        95,
+      );
+    },
+    TEST_TIMEOUT,
+  );
 
-  test('should divide two numbers', () => {
-    // Write your test here
-  });
+  test(
+    'should divide two numbers',
+    () => {
+      expect(simpleCalculator({ a: 20, b: 5, action: Action.Divide })).toBe(4);
+    },
+    TEST_TIMEOUT,
+  );
 
-  test('should exponentiate two numbers', () => {
-    // Write your test here
-  });
+  test(
+    'should exponentiate two numbers',
+    () => {
+      expect(
+        simpleCalculator({ a: 2, b: 6, action: Action.Exponentiate }),
+      ).toBe(64);
+    },
+    TEST_TIMEOUT,
+  );
 
-  test('should return null for invalid action', () => {
-    // Write your test here
-  });
+  test(
+    'should return null for invalid action',
+    () => {
+      expect(
+        simpleCalculator({
+          a: 2,
+          b: 6,
+          action: 'Give me one billion bucks, please',
+        }),
+      ).toBeNull();
+    },
+    TEST_TIMEOUT,
+  );
 
-  test('should return null for invalid arguments', () => {
-    // Write your test here
-  });
+  test(
+    'should return null for invalid arguments',
+    () => {
+      expect(
+        simpleCalculator({
+          a: 'two',
+          b: 6,
+          action: Action.Add,
+        }),
+      ).toBeNull();
+    },
+    TEST_TIMEOUT,
+  );
 });
